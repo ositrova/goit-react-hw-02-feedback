20d02b752dbd5337dd61d2647d08fa7ef5719627
ositrova <110326755+ositrova@users.noreply.github.com>
2023-02-23 11:38:29 +0200

    Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+
+import { Feedback } from "./Feedback/Feedback";
+import { GlobalStyle } from "./GlobalStyle";
+import { Layout } from "./Layout/Layout";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Feedback />
+      <GlobalStyle />
+    </Layout>
   );
 };
